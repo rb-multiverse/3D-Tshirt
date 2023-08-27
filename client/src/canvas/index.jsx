45aa521ce2268863f86636c600869ef394c40fdc
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-08-26 20:49:06 -0700

    updates threejs camera

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,12 +10,12 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5}/>
       <Environment preset="city"/>
 
-      {/* <CameraRig>
-        <Backdrop/> */}
+      <CameraRig>
+        {/* <Backdrop/>  */}
         <Center>
           <Shirt/>
         </Center>
-      {/* </CameraRig> */}
+       </CameraRig>
     </Canvas>
   )
 }
